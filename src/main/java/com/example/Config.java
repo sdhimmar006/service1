2803ae12b0297a09a132c315502346b8a9b597c7
Swapnil Dhimmar <sdhimmar006@gmail.com>
2022-01-24 15:16:04 +0530

    use opentelemetry

--- src/main/java/com/example/Config.java
+++ src/main/java/com/example/Config.java
@@ -1,5 +1,10 @@
 package com.example;
 
+import com.google.cloud.opentelemetry.trace.TraceConfiguration;
+import com.google.cloud.opentelemetry.trace.TraceExporter;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,8 +12,23 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class Config {
 
+//    @Value("projid")
+//    String projId;
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    @SneakyThrows
+    public SpanExporter googleTraceExporter() {
+        return TraceExporter.createWithConfiguration(
+                TraceConfiguration.builder()
+//                        .setProjectId(projId)
+                        .build()
+        );
+    }
+
+
 }
